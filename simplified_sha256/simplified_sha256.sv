93bb29354f68b14f12883c5ac4002deceed3decb
RaulDuarteIV <102642506+RaulDuarteIV@users.noreply.github.com>
2023-06-02 12:09:46 -0700

    Update simplified_sha256.sv

--- simplified_sha256/simplified_sha256.sv
+++ simplified_sha256/simplified_sha256.sv
@@ -47,12 +47,32 @@ assign tstep = (i - 1);
 // Note : Function defined are for reference purpose. Feel free to add more functions or modify below.
 // Function to determine number of blocks in memory to fetch
 function logic [15:0] determine_num_blocks(input logic [31:0] size);
-
+	
   // Student to add function implementation
-  words = size*32;
 
 endfunction
 
+
+
+
+
+
+
+
+
+
+function logic [255:0] sha256_op(input logic [31:0] a, b, c, d, e, f, g, h, w, k);
+logic [31:0] S1, S0, ch, maj, t1, t2; // internal signals
+begin
+S1 = rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
+ch = (e & f) ^ ((~e) & g);
+t1 = ch + S1 + h + k + w;
+S0 = rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
+maj = (a & b) ^ (a & c) ^ (b & c);
+t2 = maj + S0;
+sha256_op = {t1 + t2, a, b, c, d + t1, e, f, g};
+end
+endfunction
 
 // SHA256 hash round
 function logic [255:0] sha256_op(input logic [31:0] a, b, c, d, e, f, g, h, w,
